59f74e44883f742600f39569a1f1ade783e19db0
penguinboots <sabrina.ynw@gmail.com>
2023-06-02 11:53:51 -0700

    build Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.scss'
+import About from './components/About'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="app-wrapper">
+      <Navbar />
+      <Hero />
+      {/* <Menu /> */}
+      <About />
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </div>
   )
 }
 
