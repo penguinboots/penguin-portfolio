379b599fb06de950fd89c55037d17119f6e23624
penguinboots <sabrina.ynw@gmail.com>
2023-06-02 20:38:20 -0700

    adjust navbar link scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-scroll";
 export default function Navbar() {
+  function convertRemToPixels(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
+  }
   return (
     <nav className="nav-bar">
       <div className="left-nav">
@@ -12,32 +17,49 @@ export default function Navbar() {
       <div className="right-nav">
         <ul className="nav-links">
           <li className="nav-item">
-            <Link to="about-container" spy={true} smooth={true} duration={500}>
+            <Link
+              to="about-container"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-convertRemToPixels(8)}
+            >
               About
             </Link>
           </li>
+          <li className="nav-divider">|</li>
           <li className="nav-item">
             <Link
               to="projects-container"
               spy={true}
               smooth={true}
               duration={500}
+              offset={-convertRemToPixels(8)}
             >
               Projects
             </Link>
           </li>
+          <li className="nav-divider">|</li>
           <li className="nav-item">
             <Link
               to="contact-container"
               spy={true}
               smooth={true}
               duration={500}
+              offset={-convertRemToPixels(8)}
             >
               Contact
             </Link>
           </li>
+          <li className="nav-divider">|</li>
           <li className="nav-item">
-            <a href="#">Resume</a>
+            <a
+              href="https://flowcv.com/resume/7vgjugqk7r"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Resume
+            </a>
           </li>
         </ul>
       </div>
